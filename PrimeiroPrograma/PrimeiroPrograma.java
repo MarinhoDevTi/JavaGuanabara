c833d5fe1bc3693b4f2806570766706b0ad528a8
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-07-21 00:34:34 -0300

    Create PrimeiroPrograma.java

---
+++ PrimeiroPrograma/PrimeiroPrograma.java
@@ -0,0 +1,8 @@
+package primeiroprograma;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args){
+        //contudo do codigo
+        System.out.println ("Olá Mundo");
+    }
+}
